dcd0614c35de88a60175101e6482c46f11ca0860
Joe <suphawutk@gmail.com>
2017-11-30 14:46:50 +0700

    'add2'

--- src/Cell.js
+++ src/Cell.js
@@ -3,18 +3,18 @@ import React,{ Component } from 'react';
 class Cell extends Component {
   render() {
     return (
-      <button style={{
+      <div style={{
         margin:5,
         border:0,
         borderRadius:5,
         width:70,
         height:70,
         backgroundColor:this.props.color,
-        display:'inline',
+        display:'inline-block',
         color:'white',
         fontSize:'50px',
         boxShadow:' 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
-      }}>{this.props.text}</button>
+      }}>{this.props.text}</div>
     );
   }
 }
