5a02e3d50624d457f968b8cdcc66cc794670707f
Rahul gaba <rahulgaba16@gmail.com>
2017-12-01 18:00:51 +0530

    fixed tests

--- src/components/Cell/__tests__/Cell.component.test.js
+++ src/components/Cell/__tests__/Cell.component.test.js
@@ -3,11 +3,11 @@ import renderer from 'react-test-renderer';
 import Cell from '../Cell.component';
 
 test('Cell: title o', () => {
-    const snapshot = renderer.create(<Cell title='o'/>).toJSON();
+    const snapshot = renderer.create(<Cell title='o' cellClickHandler={jest.fn}/>).toJSON();
     expect(snapshot).toMatchSnapshot();
 });
 
 test('Cell: title x', () => {
-    const snapshot = renderer.create(<Cell title='x'/>).toJSON();
+    const snapshot = renderer.create(<Cell title='x' cellClickHandler={jest.fn}/>).toJSON();
     expect(snapshot).toMatchSnapshot();
 });
