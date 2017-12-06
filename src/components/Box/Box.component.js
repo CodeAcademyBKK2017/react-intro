415d9ba63f0b312089cb384ddd7fe0707761713a
Teeraphong Chaichalermpreecha <teeraphong.m@gmail.com>
2017-12-06 12:13:18 +0700

    init

--- src/components/Box/Box.component.js
+++ src/components/Box/Box.component.js
@@ -3,8 +3,24 @@ import PropTypes from 'prop-types';
 import Row from '../Row/Row.component';
 
 class Box extends React.Component {
-  cellClickHandler = (cellData) => () => {
-    console.log('cell clicked', cellData);
+  // cellClickHandler = (cellData) => () => {
+  //   console.log('cell clicked', cellData);
+  // }
+  // render () {
+  //   const {boxData} = this.props;
+  //   const rowLists = boxData.map((rowData, index) => <Row key={index} rowData={rowData} cellClickHandler={this.cellClickHandler} />);
+  //   return (
+  //     <div>
+  //       {rowLists}
+  //     </div>
+  //   );
+  // }
+  constructor () {
+    super();
+    this.cellClickHandler = this.cellClickHandler.bind(this);
+  }
+  cellClickHandler (cellData) {
+    return () => console.log('cell clicked', cellData);
   }
   render () {
     const {boxData} = this.props;
