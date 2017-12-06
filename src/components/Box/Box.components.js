544adb5727822e11e1942fd28da8e6d0634e54b0
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-06 12:13:55 +0700

    change to es6

--- src/components/Box/Box.components.js
+++ src/components/Box/Box.components.js
@@ -4,24 +4,37 @@ import Row from '../Row/Row.components';
 import noop from 'lodash/noop';
 class Box extends Component {
 
-    cellClickHandler = (cellValue) =>  // class preproties for es7 feature
-      () => {
-        console.log('cell clicked :: ', cellValue);
-      }
+  constructor () {
+    super();
+    this.cellClickHandler = this.cellClickHandler.bind(this);
+  }
+
+  // cellClickHandler = (cellValue) =>  // class preproties for es7 feature
+  //   () => {
+  //     console.log('cell clicked :: ', cellValue);
+  //   }
+
+  // class method : ES6
+  cellClickHandler (cellValue) {
+    return () => {
+      console.log('cell clicked :: ', cellValue);
+      
+    };
+  }
     
-    // cellClickHandler (cellValue){ //class method
-    //     console.log('cell clicked :: ', cellValue);
-    // }
-    render () {
-      // this.props.rowData
-      return (
-        <div>
-          <Row cellData={  this.props.rowData[0] } cellClickHandler={this.cellClickHandler}/> 
-          <Row cellData={  this.props.rowData[1] } cellClickHandler={this.cellClickHandler}/> 
-          <Row cellData={  this.props.rowData[2] } cellClickHandler={this.cellClickHandler}/>
-        </div>
-      );
-    }
+  // cellClickHandler (cellValue){ //class method
+  //     console.log('cell clicked :: ', cellValue);
+  // }
+  render () {
+    // this.props.rowData
+    return (
+      <div>
+        <Row cellData={  this.props.rowData[0] } cellClickHandler={this.cellClickHandler}/> 
+        <Row cellData={  this.props.rowData[1] } cellClickHandler={this.cellClickHandler}/> 
+        <Row cellData={  this.props.rowData[2] } cellClickHandler={this.cellClickHandler}/>
+      </div>
+    );
+  }
 }
 
 
