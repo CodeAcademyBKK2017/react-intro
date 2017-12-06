f6bac2a0065b083183e8fef669138004a595cce8
deathsky0725 <wbenjapol@gmail.com>
2017-12-06 07:46:44 +0000

    Change to Functional Component In React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Box from './components/Box/Box.component';
 class App extends Component {
-  render() {
-    const bData = [['O','X','O'],['X','O','O'],['X','X','O']]
+  render () {
+    const bData = [['O','X','O'],['X','O','O'],['X','X','O']];
     return (
       <div>
         <Box boxData={bData} />
--- src/components/Cell/Cell.component.js
+++ src/components/Cell/Cell.component.js
@@ -2,17 +2,21 @@ import React, {Component} from 'react';
 import './Cell.style.css';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
-class Cell extends Component {
+// class Cell extends Component {
   
-  render () {
-    const value = this.props.value.toUpperCase();
-    return (
-      <div className="Cell" onClick={this.props.cellclickHandler(value)}>
-        {value}
-      </div>
-    );
-  }
-}
+//   render () {
+//     const value = this.props.value.toUpperCase();
+//     return (
+//       <div className="Cell" onClick={this.props.cellclickHandler(value)}>
+//         {value}
+//       </div>
+//     );
+//   }
+// }
+const Cell = ({value,cellclickHandler}) => (
+  <div className="Cell" onClick={cellclickHandler(value)}>{value}</div>
+);
+
 Cell.propTypes = {
   cellclickHandler: PropTypes.func.isRequired,
   value: PropTypes.oneOf(['X','O']).isRequired
