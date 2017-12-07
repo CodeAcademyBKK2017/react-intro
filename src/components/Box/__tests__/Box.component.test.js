3a4da5e002a28af5838a011db45d76e4aa499803
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-07 11:32:45 +0700

    refactor test

--- src/components/Box/__tests__/Box.component.test.js
+++ src/components/Box/__tests__/Box.component.test.js
@@ -2,48 +2,54 @@ import React from 'react';
 import Box from '../Box.component';
 import Row from '../../Row/Row.component';
 import renderer from 'react-test-renderer';
-import uuid from 'uuid';
 import {shallow} from 'enzyme';
 
-const data = [
-	{
-		rowId: uuid(),
-		data: ["O", "X", "O"]
-	},
-	{
-		rowId: uuid(),
-		data: ["X", "O", "O"]
-	},
-	{
-		rowId: uuid(),
-		data: ["X", "X", "O"]
-	}
-];
+describe('Box Component', () => {
 
-test('Box', () => {
-	const snapshot = renderer.create(<Box rowData={data} />).toJSON();
-	expect(snapshot).toMatchSnapshot();
-});
-
-test('Box: click handler', () => {
-	const wrapper = shallow(<Box rowData={data} />);
-	const instance = wrapper.instance();
+	let data = [];
+	let tmpBoxComp;
+	let instance = null;
 	
-	const text = 'T';
-	const logContainerFunc = instance.cellClickHandler(text);
-	expect(logContainerFunc()).toBe(text);
-});
+	beforeEach(() => {
+		data = [
+			{
+				rowId: "R001",
+				data: ["O", "X", "O"]
+			},
+			{
+				rowId: "R002",
+				data: ["X", "O", "O"]
+			},
+			{
+				rowId: "R003",
+				data: ["X", "X", "O"]
+			}
+		];
+		tmpBoxComp = <Box rowData={data} />;
+		
+		const wrapper = shallow(tmpBoxComp);
+		instance = wrapper.instance();
+	});
 
-test('Box: get row component', () => {
-	const wrapper = shallow(<Box rowData={data} />);
-	const instance = wrapper.instance();
+	it('render correctly', () => {
+		const snapshot = renderer.create(tmpBoxComp).toJSON();
+		expect(snapshot).toMatchSnapshot();
+	});
 	
-	const rowData = {
-		rowId: "id001",
-		data: ["O", "X", "O"]
-	};
-	const rowComp = instance.mapRowWithKey(rowData);
-
-	const expected = <Row key={rowData.rowId} cellData={rowData.data} cellClickHandler={instance.cellClickHandler}/>;
-	expect(rowComp).toEqual(expected);
+	it('cell click handler', () => {
+		const text = 'T';
+		const logContainerFunc = instance.cellClickHandler(text);
+		expect(logContainerFunc()).toBe(text);
+	});
+	
+	it('get row component', () => {
+		const rowData = {
+			rowId: "id001",
+			data: ["O", "X", "O"]
+		};
+		const rowComp = instance.mapRowWithKey(rowData);
+	
+		const expected = <Row key={rowData.rowId} cellData={rowData.data} cellClickHandler={instance.cellClickHandler}/>;
+		expect(rowComp).toEqual(expected);
+	});
 });
