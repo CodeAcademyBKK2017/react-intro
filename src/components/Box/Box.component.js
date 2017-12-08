94d6532a021e4e0e8f14d942b36d8b774f5bcb90
Rahul gaba <rahulgaba16@gmail.com>
2017-12-08 13:57:07 +0700

    Done exercise: print x on click on call

--- src/components/Box/Box.component.js
+++ src/components/Box/Box.component.js
@@ -1,13 +1,9 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import Row from '../Row/Row.component';
-import cloneDeep from 'lodash/cloneDeep';
 import replaceIndex from 'replace-array-index';
 import uuid from 'uuid';
 
-
-// const replaceIndex = (array, index, replaceWith) => [...array.slice(0, index), replaceWith, ...array.slice(index + 1, array.length)];
- 
 class Box extends Component {
   state = {
     bData: [
@@ -23,22 +19,16 @@ class Box extends Component {
         'items':  ['', '', ''],
         'id': uuid()
       }
-    ],
-    counter : 0
+    ]
   }
   cellclickHandler = (rowID, cellIndex) => () => {
     const foundRow =  this.state.bData.find((rowData) => rowData.id === rowID);
     const selectedRowIndex = this.state.bData.indexOf(foundRow);
-    let items = [];
-    if (this.state.counter % 2 === 0) {
-      items = replaceIndex(foundRow.items, cellIndex, 'X');
-    } else {
-      items = replaceIndex(foundRow.items, cellIndex, 'O');
-    }
+    const items = replaceIndex(foundRow.items, cellIndex, 'X');
     const updatedRow = {id: rowID, items};
     const newBoxData = replaceIndex(this.state.bData, selectedRowIndex, updatedRow);
     this.setState({'bData' :newBoxData});
-  } 
+  }
 
   createRow = (rowData) => <Row cellData={rowData.items}
     key={rowData.id} rowID={rowData.id} cellclickHandler={this.cellclickHandler} />
