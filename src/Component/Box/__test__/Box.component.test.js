1a6e87e2455da322862448a9d7cf0583aeff5f19
Panurut <nubrabra5@gmail.com>
2017-12-08 14:53:39 +0700

    '08122017V2'

--- src/Component/Box/__test__/Box.component.test.js
+++ src/Component/Box/__test__/Box.component.test.js
@@ -4,19 +4,13 @@ import renderer from 'react-test-renderer';
 import uuid from 'uuid';
 
 const valueData = [
-  {
-    value: ['x','o','o'],
-    key: uuid()
-  },{
-    value: ['x','x','o'],
-    key: uuid()
-  },{
-    value: ['o','x','o'],
-    key: uuid()
-  }
+  {value: ['x','o','o'],key: uuid()},
+  {value: ['x','x','o'],key: uuid()},
+  {value: ['o','x','o'],key: uuid()}
 ];
 
 test('Box Snapshot Test',() => {
   const tree = renderer.create(<Box valueData = {valueData} cellOnClick = {jest.fn()}/>);
   expect(tree).toMatchSnapshot();
 });
+
