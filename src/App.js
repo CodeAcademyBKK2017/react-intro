e9ca6d328a6e67f701b5ae763cbdca67b36d073a
unknown <suphawutk@gmail.com>
2017-12-12 05:56:43 +0000

    Cool

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+        <div className="HeaderClass"><b>TIC</b></div>
+        <div className="HeaderClass"><b>TAC</b></div>
+        <div className="HeaderClass"><b>TOE</b></div>
         <div className="App-intro">
-          <Box/>
+          <div class="boxCss">
+            <Box/>
+          </div>
         </div>
-        <div className="KD">Made By KingDark</div>
+        <div className="KD"><span>Made By</span> <b>KingDark</b></div>
       </div>
     );
   }
