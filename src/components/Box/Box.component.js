a76dd8a7680a78378c5a22b67cc593974ff6e73b
Jack <jakkrit.chin@gmail.com>
2017-12-12 11:46:47 +0700

    Wow

--- src/components/Box/Box.component.js
+++ src/components/Box/Box.component.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import Row from '../Row/Row.component';
 import replaceArrayIndex from 'replace-array-index';
+import swal from 'sweetalert';
 
 class Box extends Component {
-  state = {
+  initialState = {
     currentPlayer: 'o',
     gameData: [['','',''],['','',''],['','','']],
     winner: null,
     gameOver: false
   }
+
+  state = this.initialState;
 
   cellClickHandler = (pos) => () => {
     const {gameData, currentPlayer, gameOver, winner} = this.state;
@@ -18,12 +21,24 @@ class Box extends Component {
     const newGameData = replaceArrayIndex(gameData, pos.row, newGameRowData);
     newGameData[pos.row][pos.col] = currentPlayer;
 
-    this.setState({
-      gameData: newGameData, 
-      currentPlayer: currentPlayer === 'o' ? 'x' : 'o',
-      winner: this.getWinner(newGameData, pos, currentPlayer),
-      gameOver: this.isGameOver(newGameData),
-    });
+    const theWinner = this.getWinner(newGameData, pos, currentPlayer);
+    const isGameOver = this.isGameOver(newGameData);
+
+    if (theWinner || isGameOver) {
+      let message = '';
+      if (theWinner) message = `${theWinner.toUpperCase()} WINNER !!!`;
+      if (isGameOver) message = 'O X DRAW';
+
+      swal(message);
+      this.resetGame();
+    } else {
+      this.setState({
+        gameData: newGameData, 
+        currentPlayer: currentPlayer === 'o' ? 'x' : 'o',
+        winner: theWinner,
+        gameOver: isGameOver,
+      });
+    }
   }
 
   getWinner = (gameData, pos, currentPlayer) => {
@@ -35,7 +50,6 @@ class Box extends Component {
     ];
 
     let theWinner = null;
-
     winnerRules.every((winnerRule) => {
       const [a,b,c] = winnerRule;
       const line = `${gameData[a.row][a.col]}${gameData[b.row][b.col]}${gameData[c.row][c.col]}`;
@@ -52,55 +66,20 @@ class Box extends Component {
 
   isGameOver = (gameData) => gameData.every((row) => row.every((cell) => cell !== ''))
 
-  resetGame = () => {
-    this.setState({
-      currentPlayer: 'o',
-      gameData: [['','',''],['','',''],['','','']],
-      winner: null,
-      gameOver: false
-    });
-  }
+  resetGame = () => this.setState(this.initialState);
 
   getRow = (rowData, i) => <Row data={rowData} rowIndex={i} key={`r${i}`} cellClickHandler={this.cellClickHandler} />
 
   getRestartButton = () => <button onClick={this.resetGame}>Restart Game</button>;
 
-  getStatusPanel = (gameOver, winner, currentPlayer) => {
-    let statusPanel = null;
-
-    if (winner) {
-      statusPanel = (
-        <div>
-          <p><b>{winner.toUpperCase()}</b> WINNER !!!</p>
-          <p>{this.getRestartButton()}</p>
-        </div>
-      );
-    } else if (gameOver) {
-      statusPanel = (
-        <div>
-          <p><b>O X</b> DRAW</p>
-          <p>{this.getRestartButton()}</p>
-        </div>
-      );
-    } else {
-      statusPanel = (<p><b>{currentPlayer.toUpperCase()}</b> Turn</p>);
-    }
-
-    return statusPanel;
-  }
-
   render () {
-    const {gameData, winner, gameOver, currentPlayer} = this.state;
+    const {gameData} = this.state;
     const rows = gameData.map(this.getRow);
-    const statusPanel = this.getStatusPanel(gameOver, winner, currentPlayer);
-    
+
     return (
       <div>
         <h2>Tie Tac Toe</h2>
-        {statusPanel}
-        <div>
-          {rows}
-        </div>
+        {rows}
       </div>
     );
   }
